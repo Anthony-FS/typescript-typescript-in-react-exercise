541936153988724607247e382b0f3b090bc5b634
AnthonyFS <anthonyfungesmith@gmail.com>
2026-07-14 17:43:20 +0700

    completed assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return (
+    <div>
+      <Greeting name="John" age={20} />
+    </div>
+  );
 }
 
 export default App;
